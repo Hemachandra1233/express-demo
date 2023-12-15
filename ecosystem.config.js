f989d3c6349e6cc83d5cf1b1e106ddb825d2fbde
Hemachandra1233 <hemachandrachindam@gmail.com>
2023-12-15 13:31:02 +0530

    added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,8 +11,8 @@ module.exports = {
        "user" : "ubuntu",
        "host" : ["127.0.0.1"],
        "ref"  : "origin/master",
-       "repo" : "git@github.com:Hemachandra1233/pm2-demo.git",
-       "path" : "/home/ubuntu/express-demo",
+       "repo" : "git@github.com:Hemachandra1233/express-demo.git",
+       "path" : "/home/ubuntu/pm2-demo",
        "post-deploy" : "npm pack"
     }
   }
